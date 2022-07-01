fc9fc5baac2a44fffee0a9cd10421cf90f779cc8
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-07-01 01:07:12 -0500

    Update Util.cs

--- Source/Util.cs
+++ Source/Util.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/* Solar Science (SOL)
+ * Solar Science is an add-on by Logan Hardin, also known as Snoopy 20111, for Kerbal Space Program by Squad.
+ * "I would prefer to be referred to as Snoopy 20111 in any attributions. Let's be honest, I'm not going to track you down, I would just appreciate it."
+ * Praise the Sun
+ 
+ * Copyright (C) 2015, 2016 Snoopy 20111 (Logan Hardin)
+ * Copyright (C) 2019, 2022 zer0Kerbal (zer0Kerbal at hotmail dot com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
